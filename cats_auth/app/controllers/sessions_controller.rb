ba5716b9f21787b529dcf6fb159e8db304f007e5
Onyi Cho <onyicho@gmail.com>
2019-06-05 14:45:27 -0700

    Add methods for Users Controller and Sessions Controller

--- cats_auth/app/controllers/sessions_controller.rb
+++ cats_auth/app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+        params[:users][:user_name],
+        params[:users][:password]
+      )
+    if @user
+      login!(@user)
+      redirect_to cats_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    # todo
+    logout!
+    # When user logout, redirect to login page
+    redirect_to new_session_url
+  end
 end
